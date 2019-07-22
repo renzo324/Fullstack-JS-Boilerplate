7dfce8f401d6b5d955709d7cf6b3fb973ba40d5f
Lorenzo Ibay <finnthehuman324@gmail.com>
2019-07-23 03:37:01 +0800

    linter configuration

--- Node/app.js
+++ Node/app.js
@@ -1,22 +1,23 @@
-const mysql = require('mysql');
-//connection
-const conn= mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: ""
-    // database: ""
+const mysql = require("mysql");
+// connection
+const conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: ""
+  // database: ""
 });
 
-//Connection
-conn.connect(function(err){
-//check for errors
-if (err) throw err;
-//else do database things
-console.log("Connection complete");
+// Connection
+conn.connect(function(err) {
+  // check for errors
+  if (err) throw err;
+  // else do database things
+  console.log("Connection complete");
 
-//create database
-conn.query("CREATE DATABASE nodeSQL", function(err,result){
+  // create database
+  // eslint-disable-next-line no-shadow
+  conn.query("CREATE DATABASE nodeSQL", function(err, result) {
     if (err) throw err;
-    console.log("database created!");
-})
+    console.log(`database created!${result}`);
+  });
 });
