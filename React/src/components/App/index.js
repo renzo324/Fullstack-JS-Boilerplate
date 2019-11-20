5a631c68797cc77044dcf83eff90b303d30583b7
Lorenzo Ibay <finnthehuman324@gmail.com>
2019-11-21 03:03:25 +0800

    update react boilerplate

---
+++ React/src/components/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HomePage from '../Home';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
